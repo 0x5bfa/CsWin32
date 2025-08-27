0f542ce3d0cfb2ca1a7634d500b77c0d6dda737e
0x5BFA <62196528+0x5bfa@users.noreply.github.com>
2025-08-27 20:38:41 +0900

    Simplified SourceGenerator.Execute

--- src/Files.CsWin32/SourceGenerator.cs
+++ src/Files.CsWin32/SourceGenerator.cs
@@ -1,15 +1,15 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using System.Text.Json;
 using Microsoft.CodeAnalysis.Text;
+using System.Text.Json;
 
 namespace Files.CsWin32;
 
 /// <summary>
 /// Generates the source code for the p/invoke methods and supporting types into some C# project.
 /// </summary>
-[Generator]
+[Generator()]
 public class SourceGenerator : ISourceGenerator
 {
 	private const string NativeMethodsTxtAdditionalFileName = "NativeMethods.txt";
@@ -29,24 +29,53 @@ public class SourceGenerator : ISourceGenerator
 	};
 
 	/// <inheritdoc/>
-	public void Initialize(GeneratorInitializationContext context)
-	{
-	}
+	public void Initialize(GeneratorInitializationContext context) { }
 
 	/// <inheritdoc/>
 	public void Execute(GeneratorExecutionContext context)
 	{
-		if (context.Compilation is not CSharpCompilation compilation)
+		if (context.Compilation is not CSharpCompilation compilation ||
+			GetNativeMethodsJsonOptions(context) is not { } options ||
+			GetNativeMethodsTxtFiles(context) is not { } nativeMethodsTxtFiles ||
+			!nativeMethodsTxtFiles.Any())
+			return;
+
+		if (!compilation.Options.AllowUnsafe)
+			context.ReportDiagnostic(Diagnostic.Create(DiagnosticDescriptors.UnsafeCodeRequired, location: null));
+
+		if (compilation.GetTypeByMetadataName("System.Memory`1") is null)
+			context.ReportDiagnostic(Diagnostic.Create(DiagnosticDescriptors.MissingRecommendedReference, location: null, "System.Memory"));
+
+		// Create a generator for each metadata path & a generator for the documentations of the APIs in those metadata.
+		IEnumerable<string> appLocalLibraries = CollectAppLocalAllowedLibraries(context);
+		Docs? docs = ParseDocs(context);
+		Generator[] generators = [.. CollectMetadataPaths(context).Select(path => new Generator(path, docs, appLocalLibraries, options, compilation, (CSharpParseOptions)context.ParseOptions))];
+		if (TryFindNonUniqueValue(generators, g => g.InputAssemblyName, StringComparer.OrdinalIgnoreCase, out (Generator Item, string Value) nonUniqueGenerator))
 		{
+			context.ReportDiagnostic(Diagnostic.Create(DiagnosticDescriptors.NonUniqueMetadataInputs, null, nonUniqueGenerator.Value));
 			return;
 		}
 
+		// Generate the syntax trees for all requested APIs
+		using SuperGenerator superGenerator = SuperGenerator.Combine(generators);
+		foreach (AdditionalText nativeMethodsTxtFile in nativeMethodsTxtFiles)
+			GenerateAll(context, superGenerator, nativeMethodsTxtFile, options);
+
+		// Produce C# source files from the accumulated compilation units.
+		foreach (KeyValuePair<string, CompilationUnitSyntax> unit in superGenerator.GetCompilationUnits(context.CancellationToken))
+			context.AddSource(unit.Key, unit.Value.GetText(Encoding.UTF8));
+	}
+
+	private static GeneratorOptions? GetNativeMethodsJsonOptions(GeneratorExecutionContext context)
+	{
 		GeneratorOptions options;
 		AdditionalText? nativeMethodsJsonFile = context.AdditionalFiles
 			.FirstOrDefault(af => string.Equals(Path.GetFileName(af.Path), NativeMethodsJsonAdditionalFileName, StringComparison.OrdinalIgnoreCase));
-		if (nativeMethodsJsonFile is object)
+
+		if (nativeMethodsJsonFile is not null)
 		{
 			string optionsJson = nativeMethodsJsonFile.GetText(context.CancellationToken)!.ToString();
+
 			try
 			{
 				options = JsonSerializer.Deserialize<GeneratorOptions>(optionsJson, JsonOptions);
@@ -54,7 +83,7 @@ public class SourceGenerator : ISourceGenerator
 			catch (JsonException ex)
 			{
 				context.ReportDiagnostic(Diagnostic.Create(DiagnosticDescriptors.OptionsParsingError, location: null, nativeMethodsJsonFile.Path, ex.Message));
-				return;
+				return null;
 			}
 		}
 		else
@@ -62,151 +91,14 @@ public class SourceGenerator : ISourceGenerator
 			options = new GeneratorOptions();
 		}
 
+		return options;
+	}
+
+	private static IEnumerable<AdditionalText> GetNativeMethodsTxtFiles(GeneratorExecutionContext context)
+	{
 		IEnumerable<AdditionalText> nativeMethodsTxtFiles = context.AdditionalFiles
 			.Where(af => string.Equals(Path.GetFileName(af.Path), NativeMethodsTxtAdditionalFileName, StringComparison.OrdinalIgnoreCase));
-		if (!nativeMethodsTxtFiles.Any())
-		{
-			return;
-		}
-
-		var parseOptions = (CSharpParseOptions)context.ParseOptions;
-
-		if (!compilation.Options.AllowUnsafe)
-		{
-			context.ReportDiagnostic(Diagnostic.Create(DiagnosticDescriptors.UnsafeCodeRequired, location: null));
-		}
-
-		if (compilation.GetTypeByMetadataName("System.Memory`1") is null)
-		{
-			context.ReportDiagnostic(Diagnostic.Create(DiagnosticDescriptors.MissingRecommendedReference, location: null, "System.Memory"));
-		}
-
-		IEnumerable<string> appLocalLibraries = CollectAppLocalAllowedLibraries(context);
-		Docs? docs = ParseDocs(context);
-		Generator[] generators = CollectMetadataPaths(context).Select(path => new Generator(path, docs, appLocalLibraries, options, compilation, parseOptions)).ToArray();
-		if (TryFindNonUniqueValue(generators, g => g.InputAssemblyName, StringComparer.OrdinalIgnoreCase, out (Generator Item, string Value) nonUniqueGenerator))
-		{
-			context.ReportDiagnostic(Diagnostic.Create(DiagnosticDescriptors.NonUniqueMetadataInputs, null, nonUniqueGenerator.Value));
-			return;
-		}
-
-		using SuperGenerator superGenerator = SuperGenerator.Combine(generators);
-		foreach (AdditionalText nativeMethodsTxtFile in nativeMethodsTxtFiles)
-		{
-			SourceText? nativeMethodsTxt = nativeMethodsTxtFile.GetText(context.CancellationToken);
-			if (nativeMethodsTxt is null)
-			{
-				return;
-			}
-
-			foreach (TextLine line in nativeMethodsTxt.Lines)
-			{
-				context.CancellationToken.ThrowIfCancellationRequested();
-				string name = line.ToString();
-				if (string.IsNullOrWhiteSpace(name) || name.StartsWith("//", StringComparison.InvariantCulture))
-				{
-					continue;
-				}
-
-				name = name.Trim().Trim(ZeroWhiteSpace);
-				var location = Location.Create(nativeMethodsTxtFile.Path, line.Span, nativeMethodsTxt.Lines.GetLinePositionSpan(line.Span));
-				try
-				{
-					if (Generator.GetBannedAPIs(options).TryGetValue(name, out string? reason))
-					{
-						context.ReportDiagnostic(Diagnostic.Create(DiagnosticDescriptors.BannedApi, location, reason));
-						continue;
-					}
-
-					if (name.EndsWith(".*", StringComparison.Ordinal))
-					{
-						string? moduleName = name.Substring(0, name.Length - 2);
-						int matches = superGenerator.TryGenerateAllExternMethods(moduleName, context.CancellationToken);
-						switch (matches)
-						{
-							case 0:
-								context.ReportDiagnostic(Diagnostic.Create(DiagnosticDescriptors.NoMethodsForModule, location, moduleName));
-								break;
-							case > 1:
-								// Stop complaining about multiple metadata exporting methods from the same module.
-								// https://github.com/microsoft/CsWin32/issues/1201
-								////context.ReportDiagnostic(Diagnostic.Create(AmbiguousMatchError, location, moduleName));
-								break;
-						}
-
-						continue;
-					}
-
-					superGenerator.TryGenerate(name, out IReadOnlyCollection<string> matchingApis, out IReadOnlyCollection<string> redirectedEnums, context.CancellationToken);
-					foreach (string declaringEnum in redirectedEnums)
-					{
-						context.ReportDiagnostic(Diagnostic.Create(DiagnosticDescriptors.UseEnumValueDeclaringType, location, declaringEnum));
-					}
-
-					switch (matchingApis.Count)
-					{
-						case 0:
-							ReportNoMatch(location, name);
-							break;
-						case > 1:
-							context.ReportDiagnostic(Diagnostic.Create(DiagnosticDescriptors.AmbiguousMatchErrorWithSuggestions, location, name, ConcatSuggestions(matchingApis)));
-							break;
-					}
-				}
-				catch (GenerationFailedException ex)
-				{
-					if (Generator.IsPlatformCompatibleException(ex))
-					{
-						context.ReportDiagnostic(Diagnostic.Create(DiagnosticDescriptors.CpuArchitectureIncompatibility, location));
-					}
-					else
-					{
-						// Build up a complete error message.
-						context.ReportDiagnostic(Diagnostic.Create(DiagnosticDescriptors.InternalError, location, AssembleFullExceptionMessage(ex)));
-					}
-				}
-			}
-		}
-
-		foreach (KeyValuePair<string, CompilationUnitSyntax> unit in superGenerator.GetCompilationUnits(context.CancellationToken))
-		{
-			context.AddSource(unit.Key, unit.Value.GetText(Encoding.UTF8));
-		}
-
-		string ConcatSuggestions(IReadOnlyCollection<string> suggestions)
-		{
-			var suggestionBuilder = new StringBuilder();
-			int i = 0;
-			foreach (string suggestion in suggestions)
-			{
-				if (++i > 0)
-				{
-					suggestionBuilder.Append(i < suggestions.Count - 1 ? ", " : " or ");
-				}
-
-				suggestionBuilder.Append('"');
-				suggestionBuilder.Append(suggestion);
-				suggestionBuilder.Append('"');
-			}
-
-			return suggestionBuilder.ToString();
-		}
-
-		void ReportNoMatch(Location? location, string failedAttempt)
-		{
-			IReadOnlyList<string> suggestions = superGenerator.GetSuggestions(failedAttempt);
-			if (suggestions.Count > 0)
-			{
-				context.ReportDiagnostic(Diagnostic.Create(DiagnosticDescriptors.NoMatchingMethodOrTypeWithSuggestions, location, failedAttempt, ConcatSuggestions(suggestions)));
-			}
-			else
-			{
-				context.ReportDiagnostic(Diagnostic.Create(
-					Generator.ContainsIllegalCharactersForAPIName(failedAttempt) ? DiagnosticDescriptors.NoMatchingMethodOrTypeWithBadCharacters : DiagnosticDescriptors.NoMatchingMethodOrType,
-					location,
-					failedAttempt));
-			}
-		}
+		return nativeMethodsTxtFiles;
 	}
 
 	private static string AssembleFullExceptionMessage(Exception ex)
@@ -272,6 +164,95 @@ public class SourceGenerator : ISourceGenerator
 		return delimitedAppLocalLibraryPaths.Split('|').Select(Path.GetFileName);
 	}
 
+	private static void GenerateAll(GeneratorExecutionContext context, SuperGenerator superGenerator, AdditionalText nativeMethodsTxtFile, GeneratorOptions options)
+	{
+		SourceText? nativeMethodsTxt = nativeMethodsTxtFile.GetText(context.CancellationToken);
+		if (nativeMethodsTxt is null)
+			return;
+
+		// Go through each line of the file and generate it.
+		foreach (TextLine line in nativeMethodsTxt.Lines)
+		{
+			context.CancellationToken.ThrowIfCancellationRequested();
+
+			string name = line.ToString();
+
+			// Skip blank lines and comment lines.
+			if (string.IsNullOrWhiteSpace(name) || name.StartsWith("//", StringComparison.InvariantCulture))
+				continue;
+
+			// Remove ordinal whitespaces and ZWSP characters.
+			name = name.Trim().Trim(ZeroWhiteSpace);
+
+			// Get the location of the line for diagnostics.
+			var location = Location.Create(nativeMethodsTxtFile.Path, line.Span, nativeMethodsTxt.Lines.GetLinePositionSpan(line.Span));
+
+			try
+			{
+				// If the API requested is deprecated or not recommended, report that and move on.
+				if (Generator.GetBannedAPIs(options).TryGetValue(name, out string? reason))
+				{
+					context.ReportDiagnostic(Diagnostic.Create(DiagnosticDescriptors.BannedApi, location, reason));
+					continue;
+				}
+
+				// If the name ends with "<module name>.*", generate all methods from the module (e.g., "Shell32.*").
+				if (name.EndsWith(".*", StringComparison.Ordinal))
+				{
+					string? moduleName = name[..^2];
+					int matches = superGenerator.TryGenerateAllExternMethods(moduleName, context.CancellationToken);
+					switch (matches)
+					{
+						case 0:
+							context.ReportDiagnostic(Diagnostic.Create(DiagnosticDescriptors.NoMethodsForModule, location, moduleName));
+							break;
+						case > 1:
+							// Stop complaining about multiple metadata exporting methods from the same module.
+							// https://github.com/microsoft/CsWin32/issues/1201
+							////context.ReportDiagnostic(Diagnostic.Create(AmbiguousMatchError, location, moduleName));
+							break;
+					}
+
+					continue;
+				}
+
+				// Now, the name should be a method or type name. Generate it.
+				superGenerator.TryGenerate(name, out IReadOnlyCollection<string> matchingApis, out IReadOnlyCollection<string> redirectedEnums, context.CancellationToken);
+				foreach (string declaringEnum in redirectedEnums) context.ReportDiagnostic(Diagnostic.Create(DiagnosticDescriptors.UseEnumValueDeclaringType, location, declaringEnum));
+				switch (matchingApis.Count)
+				{
+					case 0:
+						IReadOnlyList<string> suggestions = superGenerator.GetSuggestions(name);
+						switch (suggestions.Count)
+						{
+							case > 0:
+								context.ReportDiagnostic(Diagnostic.Create(DiagnosticDescriptors.NoMatchingMethodOrTypeWithSuggestions, location, name, CombineDiagnosticSuggestions(suggestions)));
+								break;
+							default:
+								context.ReportDiagnostic(Diagnostic.Create(Generator.ContainsIllegalCharactersForAPIName(name) ? DiagnosticDescriptors.NoMatchingMethodOrTypeWithBadCharacters : DiagnosticDescriptors.NoMatchingMethodOrType, location, name));
+								break;
+						}
+						break;
+					case > 1:
+						context.ReportDiagnostic(Diagnostic.Create(DiagnosticDescriptors.AmbiguousMatchErrorWithSuggestions, location, name, CombineDiagnosticSuggestions(matchingApis)));
+						break;
+				}
+			}
+			catch (GenerationFailedException ex)
+			{
+				if (Generator.IsPlatformCompatibleException(ex))
+				{
+					context.ReportDiagnostic(Diagnostic.Create(DiagnosticDescriptors.CpuArchitectureIncompatibility, location));
+				}
+				else
+				{
+					// Build up a complete error message.
+					context.ReportDiagnostic(Diagnostic.Create(DiagnosticDescriptors.InternalError, location, AssembleFullExceptionMessage(ex)));
+				}
+			}
+		}
+	}
+
 	private static Docs? ParseDocs(GeneratorExecutionContext context)
 	{
 		Docs? docs = null;
@@ -299,5 +280,22 @@ public class SourceGenerator : ISourceGenerator
 		}
 
 		return docs;
+	}
+
+	private static string CombineDiagnosticSuggestions(IReadOnlyCollection<string> suggestions)
+	{
+		var suggestionBuilder = new StringBuilder();
+		int i = 0;
+		foreach (string suggestion in suggestions)
+		{
+			if (++i > 0)
+				suggestionBuilder.Append(i < suggestions.Count - 1 ? ", " : " or ");
+
+			suggestionBuilder.Append('"');
+			suggestionBuilder.Append(suggestion);
+			suggestionBuilder.Append('"');
+		}
+
+		return suggestionBuilder.ToString();
 	}
 }
