c854747281571ffc7cf408d0527997b6c8b8c060
0x5BFA <62196528+0x5bfa@users.noreply.github.com>
2025-08-27 22:15:37 +0900

    Added the right attribution

--- src/Files.CsWin32/Utils/MetadataFile.cs
+++ src/Files.CsWin32/Utils/MetadataFile.cs
@@ -1,5 +1,4 @@
-﻿// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+﻿// Copyright (c) 0x5BFA.
 
 using System.IO.MemoryMappedFiles;
 using System.Reflection.PortableExecutable;
